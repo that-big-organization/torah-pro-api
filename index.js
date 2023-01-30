a518aa5f2b3377ede1e84dd6cbdc108252151e8f
yyaskriloff <69407772+yyaskriloff@users.noreply.github.com>
2023-01-30 17:13:37 +0200

    update barebone schema and create custome date type

--- index.js
+++ index.js
@@ -1,39 +1,41 @@
 import { ApolloServer } from '@apollo/server';
 import { startStandaloneServer } from '@apollo/server/standalone';
+import fs from 'fs';
+import path from 'path'
+import { default as URL } from 'url';
 
-const books = [
-    {
-        title: 'The Awakening',
-        author: 'Kate Chopin',
+const __filename = URL.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+
+import { GraphQLScalarType, Kind } from 'graphql';
+
+const dateScalar = new GraphQLScalarType({
+    name: 'Date',
+    description: 'Date custom scalar type',
+    serialize(value) {
+        return value.getTime(); // Convert outgoing Date to integer for JSON
     },
-    {
-        title: 'City of Glass',
-        author: 'Paul Auster',
+    parseValue(value) {
+        return new Date(value); // Convert incoming integer to Date
     },
-];
-
-const typeDefs = `#graphql
-
-  type Book {
-    title: String
-    author: String
-  }
-
-  type Query {
-    books: [Book]
-  }
-`;
-
-const resolvers = {
-    Query: {
-        books: () => books,
+    parseLiteral(ast) {
+        if (ast.kind === Kind.INT) {
+            // Convert hard-coded AST string to integer and then to Date
+            return new Date(parseInt(ast.value, 10));
+        }
+        // Invalid hard-coded value (not an integer)
+        return null;
     },
-};
+});
 
 
 const server = new ApolloServer({
-    typeDefs,
-    resolvers,
+    typeDefs: fs.readFileSync(
+        path.join(__dirname, 'src', 'schema.graphql'),
+        'utf8'
+    ),
+    // resolvers,
 });
 
 const { url } = await startStandaloneServer(server, {
